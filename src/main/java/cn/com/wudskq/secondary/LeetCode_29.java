ba652ff554079c3cd1946bae6d179b42c42de525
chenfangchao <socrates11@foxmail.com>
2022-05-07 15:19:30 +0800

    [add]#leetcode.29两数相除(未做)

---
+++ src/main/java/cn/com/wudskq/secondary/LeetCode_29.java
@@ -0,0 +1,19 @@
+package cn.com.wudskq.secondary;
+
+/**
+ * @author chenfangchao
+ * @title: LeetCode_29
+ * @projectName leetcode-project
+ * @description: TODO 29. 两数相除
+ * @date 2022/5/3 2:36 AM
+ */
+public class LeetCode_29 {
+
+    public static void main(String[] args) {
+        int i = 100;
+
+        int i1 = i<<10;
+
+        System.out.println(i1);
+    }
+}
